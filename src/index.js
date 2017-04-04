a8d0d19d0662340cce52d12921936eeb7a708987
Askar Karasaev <askar.karasaev@gmail.com>
2017-04-04 12:36:33 +0900

    Adding modules.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { BrowserRouter, Match, Miss} from 'react-router';
+
+import App from './components/App';
 
 ReactDOM.render(
   <App />,
